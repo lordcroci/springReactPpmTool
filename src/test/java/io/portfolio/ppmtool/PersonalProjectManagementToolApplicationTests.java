5245aa0a1bb8cba5be6a4b965d3afd82d796f0a4
lcroci <loris.croci@openmindonline.it>
2020-12-14 17:35:40 +0100

    first commit

---
+++ src/test/java/io/portfolio/ppmtool/PersonalProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.portfolio.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectManagementToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
